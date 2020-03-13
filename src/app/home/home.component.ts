894902c4496913cd3f44401e2c24a0247886fb8d
Nathan <nathankfoss@gmail.com>
2020-03-13 15:08:43 -0500

    Implement programamtic routing for servers
    Also some notes about absolute/relative paths

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // When we want to do other things in addition to navigating, we implement a function to do so.
+  onLoadServers() {
+    // First we do the actual navigation.
+    this.router.navigate(['/servers'])
+
+  }
+
+  //=================================================================
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,7 +10,24 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  onReloadPage() {
+
+    this.router.navigate(['servers'])
+  
+      // Unlike the routerLink which always knows what component it resides in,
+      // the navigate method doesn't know. It will do it from the root.
+      // To remedy this we can use 'relativeTo' and 'Activated Route'
+
+      // this.router.navigate(['servers'], {relativeTo: this.activatedRoute})
+  }
+
+
+  //===========================================
+
+  constructor(private serversService: ServersService, 
+    private router: Router,
+    // private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
