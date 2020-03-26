3ef12526726c7a2b5a8ef071e82fd8b9cb2f1f97
nathan <nathankfoss@gmail.com>
2020-03-25 20:43:16 -0500

    Add comment on URL params being parsed as strings

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -17,11 +17,13 @@ export class ServerComponent implements OnInit {
   ngOnInit() {
 
     // Initial params
-    // Notice the use of '+' here, and in the subscription.
-    // This is to convert the string returned from params, into a number. We need a number because our
-    // server id's are NOT strings, they are numbers. We cannot match without the correct type.
     const id = +this.activatedRoute.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+
+    // Notice the use of '+' here, and in the subscription below.
+    // This is to convert the string returned from params, into a number. We need a number because our
+    // server id's are NOT strings, they are numbers. We cannot match without the correct type.
+    // ** FUN FACT: When we parse params from our URL, it will ALWAYS be string! **
 
     // Update page when params change
     this.activatedRoute.params.subscribe(
